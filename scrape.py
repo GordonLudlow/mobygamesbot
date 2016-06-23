157dcb4058614cc121b53fd9ed2e474478feae42
GordonLudlow <gordon@gordonludlow.com>
2016-06-22 18:26:18 -0700

    * Added dictionary that specifies which job title categories correspond to dev team titles.  For example, "Marketing" people aren't considered part of the dev team, "Art" people are. * Observe robots.txt for crawl delay (and so moby games can shut down the whole scrape operation if they want) * report size of dev team and of programming team (still need to output to csv for pretty excel charts)

--- scrape.py
+++ scrape.py
@@ -4,7 +4,30 @@
 # Scrape mobygames.com for team sizes to compare over time and by game engine
 # Once mobygames releases their API, this will all be deprecated
 
-import json, time, urllib2, HTMLParser
+import HTMLParser, json, robotparser, time, urllib2
+
+userAgent = 'Moby Games Bot (https://github.com/GordonLudlow/mobygamesbot)'
+
+# The robotparser in python 2.7 doesn't support crawl-delay, this does
+class RobotsTxtParser(robotparser.RobotFileParser):
+    def read(self):
+        """Reads the robots.txt URL and feeds it to the parser."""
+        try:
+            req = urllib2.Request(self.url, headers={'User-Agent' : userAgent})
+        except urllib2.HTTPError as err:
+            if err.code in (401, 403):
+                self.disallow_all = True
+            elif err.code >= 400:
+                self.allow_all = True
+        else:
+            response = urllib2.urlopen(req)
+            raw = response.read()
+            lines = raw.decode('utf-8').splitlines()
+            self.parse(lines)
+            for line in lines:
+                if line.startswith("Crawl-delay:"):
+                    self.crawl_delay = float(line[12:])
+
 
 class GamePageParser(HTMLParser.HTMLParser):
     def __init__(self):
@@ -29,17 +52,17 @@ class DeveloperPageParser(HTMLParser.HTMLParser):
     def handle_starttag(self, tag, attrs):
         if tag == 'h3':
             self.parsingHeader = True
-            #print "found header tag"
+            #print 'found header tag'
         elif tag == 'a':
-            #print "found anchor tag"
+            #print 'found anchor tag'
             if self.header:
                 for attr in attrs:
                     if attr[0] == 'href':
                         if self.gameLink == attr[1]:
-                            #print "found game back link"
+                            #print 'found game back link'
                             self.roles.add(self.header)
                         #else:
-                            #print "found some other a href %s (!= %s)" % (attr[1], self.gameLink)
+                            #print 'found some other a href %s (!= %s)' % (attr[1], self.gameLink)
 
     def handle_endtag(self, tag):
         if tag == 'table':
@@ -49,7 +72,9 @@ class DeveloperPageParser(HTMLParser.HTMLParser):
         if self.parsingHeader:
             self.header = data
             self.parsingHeader = False
-
+# A cached game page shouldn't get stale because MobyGames doesn't really track live teams
+# But a developer page can definitely get stale.  If the game link doesn't appear on the develop page,
+# we may have cached it before the game was added.  If that happens, we catch it below.
 
 
 def GetMobyGamePage(creditsUrl):
@@ -63,10 +88,16 @@ def GetMobyGamePage(creditsUrl):
     start = creditsUrl[:-8].rfind('/')+1
     return '/game/%s' % creditsUrl[start:-8]
 
-def GetContent(url, httpCache):
+def GetContent(url, httpCache, robot):
     if url in httpCache:
-        #print "cached"
+        #print 'cached'
         return httpCache[url]
+
+    if not robot.can_fetch(userAgent, url):
+        print 'This bot has been blocked from scraping mobygames.com by their robots.txt file'
+        print '%s is disallowed' % url
+        exit(1)
+    print 'Downloading %s' % url
     contents = ''
     try:
         # Using "Custom User-Agent" because MobyGames uses CloudFlare which blocks the default, "Python-urllib/2.6"
@@ -75,12 +106,14 @@ def GetContent(url, httpCache):
         # http://www.mobygames.com/forums/dga,2/dgb,3/dgm,223636/
         # "Talking of which, we've run into a couple of people recently who are scraping MobyGames for projects like this. Well, your scraping will soon be unnecessary! Work is officially underway on a MobyGames API - for anyone wanting to use our data for non-commercial purposes. We really want to become _the_ easily usable data source for game info, & will be talking about how you can test the system & get an API key soon."
 
-        req = urllib2.Request(url, headers={'User-Agent' : 'Moby Games Bot (https://github.com/GordonLudlow/mobygamesbot)'})
-        time.sleep(1) # TODO: read it from robots.txt (1 is the value there now)
+        time.sleep(robot.crawl_delay)
+        req = urllib2.Request(url, headers={'User-Agent' : userAgent})
         response = urllib2.urlopen(req)
-        contents = response.read()
+        encoding = response.headers.getparam('charset')
+        contents = response.read().decode(encoding)
         httpCache[url] = contents
-        httpCache['dirty'] = True
+        with open('http_cache.json', 'w') as data_file:
+            json.dump(httpCache, data_file)
     except urllib2.HTTPError, e:
         print 'Http error code - %s. %s' % (e.code, e.fp.read())
     except IOError as e:
@@ -90,34 +123,55 @@ def GetContent(url, httpCache):
         print sys.exc_info()[0]
     return contents
 
-with open('http_cache.json') as data_file:
-    httpCache = json.load(data_file)
-httpCache['dirty'] = False
-with open('games.json') as data_file:    
-    games = json.load(data_file)
-for game in games:
-    gameContent = GetContent(game['credits'], httpCache)
-    gameParser = GamePageParser()
-    gameParser.feed(gameContent)
+def main():
+    robot = RobotsTxtParser('http://www.mobygames.com/robots.txt')
+    robot.read()
 
-    print "%s credits %d people" % (game["game"], len(gameParser.teamMembers))
+    with open('http_cache.json') as data_file:
+        httpCache = json.load(data_file)
 
-    # What did these people do?
-    roles = {}
-    backlink = GetMobyGamePage(game['credits'])
-    for teamMember in gameParser.teamMembers:
-        teamMemberContent = GetContent("http://www.mobygames.com%s" % teamMember, httpCache)
-        teamMemberParser = DeveloperPageParser(backlink)
-        teamMemberParser.feed(teamMemberContent)
-        for role in teamMemberParser.roles:
-            if role in roles:
-                roles[role] = roles[role]+1
-            else:
-                roles[role] = 1
-    for role in roles:
-        print "%s: %d" % (role, roles[role])
+    with open('dev_titles.json') as data_file:
+        dev_title = json.load(data_file)
 
-# Write out any newly crawled pages to the cache
-if httpCache['dirty']:
-    with open('http_cache.json', 'w') as data_file:
-        json.dump(httpCache, data_file)
+    with open('games.json') as data_file:    
+        games = json.load(data_file)
+
+    for game in games:
+        gameContent = GetContent(game['credits'], httpCache, robot)
+        gameParser = GamePageParser()
+        gameParser.feed(gameContent)
+
+        print '%s credits %d people' % (game['game'], len(gameParser.teamMembers))
+
+        # What did these people do?
+        roles = {}
+        backlink = GetMobyGamePage(game['credits'])
+        for teamMember in gameParser.teamMembers:
+            teamMemberContent = GetContent('http://www.mobygames.com%s' % teamMember, httpCache, robot)
+            teamMemberParser = DeveloperPageParser(backlink)
+            teamMemberParser.feed(teamMemberContent)
+            if not teamMemberParser.roles:
+                print "Didn't find link back to %s from %s" % (backlink, teamMember)
+                print "Cache is stale?  Need to be able to re-cache a developer page if they work on a new game"
+                exit(1)
+            for role in teamMemberParser.roles:
+                if role in roles:
+                    roles[role].append(teamMember)
+                else:
+                    roles[role] = [teamMember]
+        devteam = set()
+        for role in roles:
+            if role not in dev_title:
+                print 'Add %s to dev_titles.json' % role
+                exit(1)
+            if dev_title[role]:
+                for person in roles[role]:
+                    devteam.add(person)
+        print '%d of these are on the dev team' % len(devteam)
+        print '%d programmers' % len(roles['Programming/Engineering'])
+
+
+if __name__=="__main__":
+    main()
+
+
