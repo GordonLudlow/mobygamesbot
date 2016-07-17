488a8c94f899532e3fed266680a1ea5123505b42
GordonLudlow <gordon@gordonludlow.com>
2016-07-17 14:21:25 -0700

    * Record which platforms a game was developed for (which may be greater than the number which have metacritic scores) * If a developer page doesn’t have a link back to the game page, check for other games with the same job title and use the role from those

--- scrape.py
+++ scrape.py
@@ -6,6 +6,7 @@
 
 import codecs, HTMLParser, json, robotparser, sys, time, urllib2
 import sqlite3
+import string
 
 userAgent = 'Moby Games Bot (https://github.com/GordonLudlow/mobygamesbot)'
 baseDeveloperUrl = 'http://www.mobygames.com/developer/sheet/view/developerId,'
@@ -38,14 +39,29 @@ class GamePageParser(HTMLParser.HTMLParser):
         self.developedByBlock = False
         self.genreBlock = False
         self.foundFirstGenre = True
+        self.platformBlock = False
+        self.platform = []
+        self.platformCount = 0
+        self.platformUrls = []
+        self.inATag = False
+        self.inPTag = False
+        self.paragraphText = ''
+        self.listingPlatformUrls = False
+        self.uninterestingCharacters = set(string.punctuation+string.whitespace)        
         HTMLParser.HTMLParser.__init__(self)
 
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
+            self.inATag = True
+            if self.platformBlock:
+                self.platform.append('')        
             for attr in attrs:
                 if attr[0] == 'href':
                     page = attr[1]
-                    if '/developer/sheet/view/developerId,' in page:
+                    if self.listingPlatformUrls:
+                        #print 'Link to platform: %s' % page
+                        self.platformUrls.append(page)
+                    elif '/developer/sheet/view/developerId,' in page:
                         if not page in self.teamMembers:
                             self.teamMembers[page] = {}
                         if not 'titles' in self.teamMembers[page]:
@@ -56,29 +72,58 @@ class GamePageParser(HTMLParser.HTMLParser):
                 if attr[0] == 'class' and attr[1] == 'crln':
                     self.newTitle = True
                     self.title = ''
+        elif tag == 'p':
+            self.inPTag = True
+            self.paragraphText = ''
 
     def handle_endtag(self, tag):
         if tag == 'td':
             self.newTitle = False
-        if tag == 'a':
+        elif tag == 'a':
+            self.inATag = False
             self.developedByBlock = False
             if self.genreBlock:
                 self.foundFirstGenre = True
-        if tag == 'div' and self.foundFirstGenre:
-            self.genreBlock = False
-            self.foundFirstGenre = False
-            try:
-                if 'DLC / Add-on' in self.genre:
-                    print self.genre
-                    exit(1)
-            except AttributeError:
-                pass
+            if self.platformBlock:
+                self.platformCount = self.platformCount + 1                
+        elif tag == 'div':
+            if self.platform:
+                self.platformBlock = False        
+            if self.foundFirstGenre:
+                self.genreBlock = False
+                self.foundFirstGenre = False
+                try:
+                    if 'DLC / Add-on' in self.genre:
+                        print self.genre
+                        exit(1)
+                except AttributeError:
+                    pass
+        elif tag == 'p':
+            self.inPTag = False
+            #print '<p>%s</p>' % self.paragraphText
+            if self.paragraphText == 'The following releases of this game have credits associated with them:':
+                self.listingPlatformUrls = True
+        elif tag == 'ul':
+            self.listingPlatformUrls = False
+                 
     def handle_data(self, data):
-        if self.newTitle:
+        if self.inPTag:
+            if self.paragraphText:
+                self.paragraphText = self.paragraphText + ' '
+            self.paragraphText = self.paragraphText + data
+        elif self.newTitle:
             if self.title:
                 self.title = self.title + ' '
             self.title = self.title + data
-        if data == 'Developed by':
+            
+        elif not self.platform and not self.inATag and (data == 'Platform' or data == 'Platforms'):
+            self.platformBlock = True
+        elif self.platformBlock and not all(c in self.uninterestingCharacters for c in data):
+            if self.platform[self.platformCount]:
+                self.platform[self.platformCount] = self.platform[self.platformCount] + ' '
+            self.platform[self.platformCount] = self.platform[self.platformCount] + data
+            
+        elif data == 'Developed by':
             self.developedByBlock = True
             self.developer = ''
         elif self.developedByBlock:
@@ -93,13 +138,18 @@ class GamePageParser(HTMLParser.HTMLParser):
                 self.genre = self.genre + ' '
             self.genre = self.genre + data
 
+            
+           
+            
 class DeveloperPageParser(HTMLParser.HTMLParser):
     def __init__(self, gameLink):
         self.gameLink = gameLink
         self.roles = set()
         self.rolesOnAllGames = {}
+        self.titlesPerRole = {}
         self.parsingHeader = False
         self.header = None
+        self.inDevCreditsTitleSpan = False
         HTMLParser.HTMLParser.__init__(self)
 
     def handle_starttag(self, tag, attrs):
@@ -131,16 +181,32 @@ class DeveloperPageParser(HTMLParser.HTMLParser):
             #    for attr in attrs:
             #        for a in attr:
             #            print a
+        elif tag=='span':
+            if self.header:
+                for attr in attrs:
+                    if attr[0] == 'class' and attr[1] == 'devCreditsTitle':
+                        self.inDevCreditsTitleSpan = True
+                        self.devCreditsTitle = ''
 
     def handle_endtag(self, tag):
         if tag == 'table':
             self.header = None
+        if tag == 'span':
+            if self.inDevCreditsTitleSpan:
+                self.inDevCreditsTitleSpan = False
+                if not self.header in self.titlesPerRole:
+                    self.titlesPerRole[self.header] = set()
+                self.titlesPerRole[self.header].add(self.devCreditsTitle)
 
     def handle_data(self, data):
         if self.parsingHeader:
             #print "header is %s" % data
             self.header = data
             self.parsingHeader = False
+        elif self.inDevCreditsTitleSpan:
+            if self.devCreditsTitle:
+                self.devCreditsTitle = self.devCreditsTitle + ' '
+            self.devCreditsTitle = self.devCreditsTitle + data
 # A cached game page shouldn't get stale because MobyGames doesn't really track live teams
 # But a developer page can definitely get stale.  If the game link doesn't appear on the develop page,
 # we may have cached it before the game was added.  If that happens, we catch it below.
@@ -153,8 +219,8 @@ def GetMobyGamePage(creditsUrl):
     # As it appears in links from a developer's page: /game/<game>
     # Example: <a href="/game/diablo-iii">
     # http://www.mobygames.com/game/windows/diablo-iii/credits
-    # http: //www.mobygames.com/game/diablo-iii
-    # 
+    # http://www.mobygames.com/game/diablo-iii
+    # /game/diablo-iii
     start = creditsUrl[:-8].rfind('/')+1
     return '/game/%s' % creditsUrl[start:-8]
 
@@ -250,10 +316,19 @@ def main():
         except KeyError:
             print "%s doesn't have metacritic scores." % game['game']
             exit(1)
+            
+    mgPlatforms = []
+    with open('platform.json') as data_file:
+        dict = json.load(data_file)
+        for p in dict:
+            if dict[p] == 'console' or dict[p] == 'desktop':
+                mgPlatforms.append(p)
 
     with codecs.open('output.txt', encoding='utf-8-sig', mode='w+') as output_file, sqlite3.connect('http_cache.db') as conn:
-        output_file.write('\t\t\t\t\t\t\tMetacritic scores\n')
+        output_file.write('\t\t\t\t\t\t\tPlatforms%sMetacritic scores\n' % ('\t' * len(mgPlatforms)))
         output_file.write('%s\t%s\t%s\t%s\t%s\t%s\t%s' % ('Game', 'Developer', 'Engine', 'Year released', 'Credited people', 'Dev team size', 'Programmers'))
+        for platform in mgPlatforms:
+            output_file.write('\t%s' % platform)        
         for platform in platforms:
             output_file.write('\t%s' % platform)
         output_file.write('\n')
@@ -264,13 +339,22 @@ def main():
             if 'ignore' in game:
                 continue
             print game['game']
-            gameContent = GetContent(game['credits'], httpCache, robot)
+            baseCreditsUrl = 'http://www.mobygames.com' + GetMobyGamePage(game['credits']) + '/credits'
+            gameContent = GetContent(baseCreditsUrl, httpCache, robot)
             gameParser = GamePageParser()
             if 'developer' in game:
                 gameParser.developer = game['developer']
             gameParser.feed(gameContent)
+            
+            # At this point, either gameParser has all of the team member data
+            # from the single-platform game -or- it has no team memeber data but
+            # has a list of urls to parse to get the data for all the platform(s).
+            # Feed it the other platform(s), if any.
+            for platformUrl in gameParser.platformUrls:
+                gameContent = GetContent('http://www.mobygames.com%s' % platformUrl, httpCache, robot)
+                gameParser.feed(gameContent)
 
-            print '%s (from %s) credits %d people' % (game['game'], gameParser.developer, len(gameParser.teamMembers))
+            print '%s (from %s) credits %d people across %d platforms (%d documented)' % (game['game'], gameParser.developer, len(gameParser.teamMembers), gameParser.platformCount, len(gameParser.platformUrls))
             output_file.write('%s\t%s\t%s\t%d\t%d' % (game['game'], gameParser.developer,game['engine'],game['year'],len(gameParser.teamMembers)))
 
             # What did these people do?
@@ -309,8 +393,19 @@ def main():
                                 print "Didn't find link back to %s from %s" % (backlink, teamMember)
                                 print "Cache is not stale!"
                                 print
-                                print teamMemberContent
-                                exit(1)
+
+                                for title in gameParser.teamMembers[teamMember]['titles']:
+                                    print title
+                                    for role in teamMemberParser.titlesPerRole:
+                                        print role
+                                        for credit in teamMemberParser.titlesPerRole[role]:
+                                            print '\t%s' % credit
+                                            if credit == title:
+                                                teamMemberParser.roles.add(role)
+                                                print '%s so assume %s' % (title, role)
+                                if not teamMemberParser.roles:
+                                    print "Couldn't find that credit on another game"
+                                    exit(1)
                 for role in teamMemberParser.roles:
                     if role in roles:
                         roles[role].append(teamMember)
@@ -349,6 +444,10 @@ def main():
                 for role in roles:
                     print '\t%s' % role
                 exit(1)
+            for platform in mgPlatforms:
+                output_file.write('\t')
+                if platform in gameParser.platform:
+                    output_file.write('1')
             for platform in platforms:
                 output_file.write('\t')
                 if platform in game['metacritic']:
